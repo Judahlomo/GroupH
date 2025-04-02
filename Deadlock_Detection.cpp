c155fb3ef490a2c211f86e438215142e61c56d47
YosepLazar <160673997+YosepLazar@users.noreply.github.com>
2025-04-02 17:12:06 -0500

    Add files via upload

---
+++ Deadlock_Detection.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unistd.h>
+#include <semaphore.h>      
+#include <pthread.h>         
+
+
+int main() {
+    bool deadlock;
+    deadlock == false;
+
+    if (deadlock == true) {
+        std::cout << "Deadlock Detected";
+    }
+    
+    return 0;
+}
